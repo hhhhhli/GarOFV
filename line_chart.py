6f9496b0b7bf003333c83270cac69c2446bfc900
sujuyu <1535481315@qq.com>
2022-09-01 11:18:21 +0800

    modify line_chart.py

--- line_chart.py
+++ line_chart.py
@@ -10,16 +10,32 @@ result_dir  = "./results"
 
 # fig=plt.figure()
 colors = ['#55112F', '#22CED1', '#0033FF', '#7FF554']
+result_content = [(' '*4).join([file.split('.')[0] for file in files])]
+
+all_accs = [[] for i in range(5, 100, 5)]
 
 for idx, file in enumerate(files):
     f = open(os.path.join(result_dir, file))
+
     acc_ = [float(line.split(',')[-1][:-1]) for line in f.readlines()]
-    acc = [(acc_[i*4] + acc_[i*4+1] + acc_[i*4+2] + acc_[i*4+3]) / 4 for i in range(len(acc_) // 4)]
-    # for i in acc:
-    #     print(i)
+    accs = [(acc_[i*4] + acc_[i*4+1] + acc_[i*4+2] + acc_[i*4+3]) / 4 for i in range(len(acc_) // 4)]
+    for i, acc in enumerate(accs):
+        all_accs[i].append(str(round(acc, 4)))
     # print("*******************")
-    plt.plot(x_axis, acc, '*--', color = colors[idx], alpha=0.5, linewidth=1, label=file.split('.')[0])
+    plt.plot(x_axis, accs, '*--', color = colors[idx], alpha=0.5, linewidth=1, label=file.split('.')[0])
     plt.legend()  #显示上面的label
+    f.close()
+
+# 保存txt file
+for line in all_accs:
+    result_content.append((' ' * 30).join(line))
+# print(result_content)
+# exit(123)
+f = open(os.path.join(result_dir, "compare.txt"), 'w')
+for line in result_content:
+    f.writelines(line + '\n')
+f.close()
+
 plt.xticks(x_axis, x_data)
 
 
